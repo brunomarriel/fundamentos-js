ef9c3296ba7124635374ab67c97d47139482be86
Bruno Marriel <brunomarriel@outlook.com>
2023-12-04 19:39:50 -0300

    Exemplos da Aula 1

---
+++ type-string.js
@@ -0,0 +1,23 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Bruno";
+console.log(citacao + meuNome);
+
+//concatenacao (+)
+
+
+
+//template string OU template literal
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
